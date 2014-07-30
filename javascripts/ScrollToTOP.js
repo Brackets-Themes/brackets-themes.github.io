7882c5cd86a3a7bc301a8e6fcbdf8c2ce3354dd4
Denisov21 <student@alice.it>
2014-07-30 23:09:14 +0200

    Scroll to TOP

---
+++ javascripts/ScrollToTOP.js
@@ -0,0 +1,18 @@
+    // scroll-to-top button show and hide
+    jQuery(document).ready(function () {
+        jQuery(window).scroll(function () {
+            //cambiare il valore 450 con quello desiderato
+            if (jQuery(this).scrollTop() > 450) {
+                jQuery('.scrollup').fadeIn();
+            } else {
+                jQuery('.scrollup').fadeOut();
+            }
+        });
+        // scroll-to-top animate
+        jQuery('.scrollup').click(function () {
+            jQuery("html, body").animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+    });
